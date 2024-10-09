a4f4fee4f58a07ad5390c9f4872610e57893c3cd
SoyAnderEscobar <andersonescobar147@gmail.com>
2024-10-09 19:26:14 -0400

    ajustes

---
+++ app/Livewire/Posts/CreateNewPost.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Posts;
+
+use Livewire\Component;
+
+class CreateNewPost extends Component
+{
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            <h1>Saludos desde newPost</h1>
+        </div>
+        HTML;
+    }
+}
